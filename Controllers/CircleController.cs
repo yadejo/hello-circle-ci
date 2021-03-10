bc7720186fe5a6cbdcc692725fd52d039d882c61
Yannick De Jonghe <yannickdejonghe@yannicks-mbp.home>
2021-03-10 17:43:10 +0100

    Add code

---
+++ Controllers/CircleController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace hello_circle_ci.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class CircleController : ControllerBase
+    {
+        private readonly ILogger<CircleController> _logger;
+
+        public CircleController(ILogger<CircleController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return @"
+                                        
+██╗░░██╗███████╗██╗░░░░░██╗░░░░░░█████╗░  ░█████╗░██╗██████╗░░█████╗░██╗░░░░░███████╗  ░█████╗░██╗
+██║░░██║██╔════╝██║░░░░░██║░░░░░██╔══██╗  ██╔══██╗██║██╔══██╗██╔══██╗██║░░░░░██╔════╝  ██╔══██╗██║
+███████║█████╗░░██║░░░░░██║░░░░░██║░░██║  ██║░░╚═╝██║██████╔╝██║░░╚═╝██║░░░░░█████╗░░  ██║░░╚═╝██║
+██╔══██║██╔══╝░░██║░░░░░██║░░░░░██║░░██║  ██║░░██╗██║██╔══██╗██║░░██╗██║░░░░░██╔══╝░░  ██║░░██╗██║
+██║░░██║███████╗███████╗███████╗╚█████╔╝  ╚█████╔╝██║██║░░██║╚█████╔╝███████╗███████╗  ╚█████╔╝██║
+╚═╝░░╚═╝╚══════╝╚══════╝╚══════╝░╚════╝░  ░╚════╝░╚═╝╚═╝░░╚═╝░╚════╝░╚══════╝╚══════╝  ░╚════╝░╚═╝
+            ";
+        }
+    }
+}
